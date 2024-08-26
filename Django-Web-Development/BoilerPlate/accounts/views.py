73e1faf3b89cdd67bc0d100bdc59ea9011c08c22
celorhm <paulbibah@hotmail.com>
2024-08-26 07:21:10 +0000

    uploading new folder changes

---
+++ Django-Web-Development/BoilerPlate/accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import UserProfile
+from .forms import SignupForm
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+
+# Create your views here.
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('home')
+
+class HomePageView(LoginRequiredMixin,TemplateView):
+    template_name = 'home.html'
+
+class ProfilePageView(LoginRequiredMixin, TemplateView):
+    template_name = 'profile-page.html'
